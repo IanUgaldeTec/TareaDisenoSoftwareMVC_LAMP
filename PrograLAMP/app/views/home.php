95d9034f206742e0b026e211342cbbce9dc0d69d
Javier184824 <leejavier62@gmail.com>
2026-05-14 00:37:33 -0600

    Update home.php

--- PrograLAMP/app/views/home.php
+++ PrograLAMP/app/views/home.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acortador de URLs - LAMP MVC</title>
-    <link rel="stylesheet" href="<?= BASE_URL ?>/css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 
